8bc7a46cdbe4b6456740d87072d0e3807fb03bb1
romanito99 <cesarbraojosglvz@gmail.com>
2021-04-10 01:53:55 +0200

    hipercubo funcional

--- src/hipercubo.c
+++ src/hipercubo.c
@@ -8,7 +8,7 @@
 #include "mpi.h"
 #include "leerFichero.h"
 
-#define D 4 //Dimensión 
+#define D 4 //Dimensión de la red 
 #define MAX_TAM 1000
 #define MAX 1000 
 
@@ -17,9 +17,9 @@ double obtenerNumeroMaximo(int rank, double bufferRank, int* vecinos);
 
 int main(int argc, char *argv[]){
     int rank, size; 
-    int numNodos = pow(2,D); // numero de procesos 
-    double bufferRank; 
-    int sigue = 0;  
+    int numNodos = pow(2,D); //indica el numero de procesos que vamos a tener dependiendo de la dimension dada 
+    double bufferRank; //buffer que almacena los rank
+    int sigue = 0; //0 si sigue la ejecucion y 1 si no. 
     int vecinos[D]; //array de vecinos de nuestro nodo
     MPI_Status status; 
     MPI_Request request;
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]){
 
     //Si soy el rank 0
     if(rank == 0){
-        //Se comprueba que se ha lanzado el numero correcto de procesos
+        
         if(numNodos != size){
             fprintf(stderr, "ERROR. Para un hipercubo de dimensión %d se deben lanzar %d procesos\n", D, numNodos); 
             
@@ -38,16 +38,16 @@ int main(int argc, char *argv[]){
             sigue = 1; 
             MPI_Bcast(&sigue, 1, MPI_INT, 0, MPI_COMM_WORLD); 
 
-        }else{ s
+        }else{ 
 
-            //Array de números del fichero
+            
             double *numleidos; 
             numleidos = malloc(MAX_TAM * sizeof(double)); 
 
-            //Leemos el fichero para obtener los valores 
+           
             int valFichero = leerFichero(numleidos); 
 
-            //Se comprueba que la cantidad de numeros leidos del fichero es correcta
+            
             if(numNodos != valFichero){
                 fprintf(stderr, "ERROR. No hay el número correcto de valores en el fichero. Hacen falta %d valores\n", numNodos); 
             
@@ -56,8 +56,10 @@ int main(int argc, char *argv[]){
                 MPI_Bcast(&sigue, 1, MPI_INT, 0, MPI_COMM_WORLD); 
 
             }else{
+                //Se continua con la ejecuion en el resto de nodos 
                 MPI_Bcast(&sigue, 1, MPI_INT, 0, MPI_COMM_WORLD);
-                //El root reparte los valores del fichero para cada uno de los nodos 
+
+                //El root reparte los valores 
                 for(int j = 0; j < valFichero; ++j){
                     bufferRank = numleidos[j]; 
                     MPI_Isend(&bufferRank, 1, MPI_DOUBLE, j, 0, MPI_COMM_WORLD,&request); 
@@ -66,15 +68,16 @@ int main(int argc, char *argv[]){
             }
         }
     }
+
     //Esperamos al root
     MPI_Bcast(&sigue, 1, MPI_INT, 0, MPI_COMM_WORLD); 
 
     if(sigue == 0){
-        //Recibo el dato
+        //Recibo mi dato
         MPI_Recv(&bufferRank, 1, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status); 
         obtenerVecinos(rank, vecinos); 
         double numMax = obtenerNumeroMaximo(rank, bufferRank, vecinos); 
-
+    
         if(rank == 0){
             printf("[RANK %d] El valor máximo de la red es: %.2f\n", rank, numMax); 
         }
@@ -99,11 +102,11 @@ void obtenerVecinos(int rank, int* vecinos){
 //Método para obtener el número máximo
 double obtenerNumeroMaximo(int rank, double bufferRank, int* vecinos){
     int i; 
-    double max; 
+    double max; //este es el máximo de mi nodo que yo le enviaré a mi vecino /*¿CUAL ES EL MAXIMO?*/ 
     MPI_Status status; 
     MPI_Request request;
 
-    for(i = 0; i < D; i++){ 
+    for(i = 0; i < D; i++){ //como tantos vecinos tenga
         if(bufferRank > max){  
             //comparo mi máximo con el valor actual del buffer
             max = bufferRank; 
